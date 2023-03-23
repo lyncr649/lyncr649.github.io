1ba7937bac43495e75caa79b89b32293ba017e0c
lyncr649 <114494322+lyncr649@users.noreply.github.com>
2023-03-23 10:48:19 -0400

    class demo

--- collectiondemo/script.js
+++ collectiondemo/script.js
@@ -1,5 +1,5 @@
 let fries = [
-    {name:"uc", url:"assets/uc.jpg"},
+    {name:"uc", url:"./assets/uc.jpg"},
     {name:"7th street", url:"./assets/7thstreet.jpg"},
     {name:"duckfat", url:"./assets/duckfat.PNG"},
     {name:"beckys", url:"./assets/beckys.PNG"},
@@ -11,8 +11,20 @@ function generateImages(fries) {
     const img = document.createElement('img')
     img.src = fries.url 
     img.classList.add('gallery-img')
+
+    // create a paragraph element 
+
+    const paragraph = document.createElement('p')
+
+    // change the inner html of the paragraph to the sign name 
+    paragraph.innerHTML = signs.name
+
+
     const galleryItem = document.createElement("div")
     galleryItem.appendChild(img)
+
+    //add the paragraph into the galleryItem div
+    galleryItem.appendChild(paragraph)
 
     document.querySelector('.gallery').appendChild(galleryItem)
 
