272ebbfed404a92c73a62c3d9c8a96977993850d
lyncr649 <114494322+lyncr649@users.noreply.github.com>
2023-03-23 16:38:30 -0400

    back to first collection

--- collection/assets/collection.js
+++ collection/assets/collection.js
@@ -1,255 +1,253 @@
-let fries = [
-	{
-		"resturant": "7th Street Burger",
-		"date": "October 29, 2022",
-		"rating" : "9.5/10",
-		"image": "./assets/fries/7thstreet.jpg",
-		"temp": "hot"
-	},
+// let fries = [
+// 	{
+// 		"resturant": "7th Street Burger",
+// 		"date": "October 29, 2022",
+// 		"rating" : "9.5/10",
+// 		"image": "./assets/fries/7thstreet.jpg",
+// 		"temp": "hot"
+// 	},
 
-	{
-		"resturant": "Bare Burger",
-		"date": "July 19, 2022",
-		"rating" : "8/10",
-		"image": "./assets/fries/bareburger.jpg",
-		"temp": "medium"
-	},
+// 	{
+// 		"resturant": "Bare Burger",
+// 		"date": "July 19, 2022",
+// 		"rating" : "8/10",
+// 		"image": "./assets/fries/bareburger.jpg",
+// 		"temp": "medium"
+// 	},
 
-	{
-		"resturant": "Beckys",
-		"date": "June 28, 2022",
-		"rating" : "9/10",
-		"image": "./assets/fries/beckys.jpg",
-		"temp": "hot"
-	},
+// 	{
+// 		"resturant": "Beckys",
+// 		"date": "June 28, 2022",
+// 		"rating" : "9/10",
+// 		"image": "./assets/fries/beckys.jpg",
+// 		"temp": "hot"
+// 	},
 
-	{
-		"resturant": "Burger King",
-		"date": "May 24, 2022",
-		"rating" : "7/10",
-		"image": "./assets/fries/burgerking.jpg",
-		"temp": "medium"
-	},
+// 	{
+// 		"resturant": "Burger King",
+// 		"date": "May 24, 2022",
+// 		"rating" : "7/10",
+// 		"image": "./assets/fries/burgerking.jpg",
+// 		"temp": "medium"
+// 	},
 
-	{
-		"resturant": "Apple Fest Fair Fries",
-		"date": "October 1, 2022",
-		"rating" : "6/10",
-		"image": "./assets/fries/cornell.jpg",
-		"temp": "hot"
-	},
+// 	{
+// 		"resturant": "Apple Fest Fair Fries",
+// 		"date": "October 1, 2022",
+// 		"rating" : "6/10",
+// 		"image": "./assets/fries/cornell.jpg",
+// 		"temp": "hot"
+// 	},
 
-	{
-		"resturant": "Louie's Lunch",
-		"date": "October 2, 2022",
-		"rating" : "9/10",
-		"image": "./assets/fries/cornell2.jpeg",
-		"temp": "hot"
-	},
+// 	{
+// 		"resturant": "Louie's Lunch",
+// 		"date": "October 2, 2022",
+// 		"rating" : "9/10",
+// 		"image": "./assets/fries/cornell2.jpeg",
+// 		"temp": "hot"
+// 	},
 
-	{
-		"resturant": "Duckfat",
-		"date": "June 17, 2022",
-		"rating" : "10/10",
-		"image": "./assets/fries/duckfat1.jpg",
-		"temp": "hot"
-	},
+// 	{
+// 		"resturant": "Duckfat",
+// 		"date": "June 17, 2022",
+// 		"rating" : "10/10",
+// 		"image": "./assets/fries/duckfat1.jpg",
+// 		"temp": "hot"
+// 	},
 
-	{
-		"resturant": "Duckfat 2",
-		"date": "June 28, 2022",
-		"rating" : "10/10",
-		"image": "./assets/fries/duckfat2.jpg",
-		"temp": "hot"
-	},
+// 	{
+// 		"resturant": "Duckfat 2",
+// 		"date": "June 28, 2022",
+// 		"rating" : "10/10",
+// 		"image": "./assets/fries/duckfat2.jpg",
+// 		"temp": "hot"
+// 	},
 
-	{
-		"resturant": "Duckfat 3",
-		"date": "March 13, 2023",
-		"rating" : "10/10",
-		"image": "./assets/fries/duckfat3.jpeg",
-		"temp": "hot"
-	},
+// 	{
+// 		"resturant": "Duckfat 3",
+// 		"date": "March 13, 2023",
+// 		"rating" : "10/10",
+// 		"image": "./assets/fries/duckfat3.jpeg",
+// 		"temp": "hot"
+// 	},
 
-	{
-		"resturant": "Lovely Day",
-		"date": "October 29, 2022",
-		"rating" : "8.5/10",
-		"image": "./assets/fries/lovelyday.jpg",
-		"temp": "hot"
-	},
+// 	{
+// 		"resturant": "Lovely Day",
+// 		"date": "October 29, 2022",
+// 		"rating" : "8.5/10",
+// 		"image": "./assets/fries/lovelyday.jpg",
+// 		"temp": "hot"
+// 	},
 
-	{
-		"resturant": "Mamouns",
-		"date": "October 28, 2022",
-		"rating" : "9/10",
-		"image": "./assets/fries/mamouns.jpg",
-		"temp": "hot"
-	},
+// 	{
+// 		"resturant": "Mamouns",
+// 		"date": "October 28, 2022",
+// 		"rating" : "9/10",
+// 		"image": "./assets/fries/mamouns.jpg",
+// 		"temp": "hot"
+// 	},
 	
-	{
-		"resturant": "McDonalds",
-		"date": "July 12, 2022",
-		"rating" : "6/10",
-		"image": "./assets/fries/mcdonalds.jpg",
-		"temp": "cold"
-	},
+// 	{
+// 		"resturant": "McDonalds",
+// 		"date": "July 12, 2022",
+// 		"rating" : "6/10",
+// 		"image": "./assets/fries/mcdonalds.jpg",
+// 		"temp": "cold"
+// 	},
 
-	{
-		"resturant": "Magnolia Café",
-		"date": "July 26, 2022",
-		"rating" : "7/10",
-		"image": "./assets/fries/nice.jpg",
-		"temp": "hot"
-	},
+// 	{
+// 		"resturant": "Magnolia Café",
+// 		"date": "July 26, 2022",
+// 		"rating" : "7/10",
+// 		"image": "./assets/fries/nice.jpg",
+// 		"temp": "hot"
+// 	},
 
-	{
-		"resturant": "Kalōs",
-		"date": "July 30, 2022",
-		"rating" : "7/10",
-		"image": "./assets/fries/nice2.jpg",
-		"temp": "medium"
-	},
+// 	{
+// 		"resturant": "Kalōs",
+// 		"date": "July 30, 2022",
+// 		"rating" : "7/10",
+// 		"image": "./assets/fries/nice2.jpg",
+// 		"temp": "medium"
+// 	},
 
-	{
-		"resturant": "Nura",
-		"date": "June 28, 2022",
-		"rating" : "7.5/10",
-		"image": "./assets/fries/nura.jpg",
-		"temp": "medium"
-	},
+// 	{
+// 		"resturant": "Nura",
+// 		"date": "June 28, 2022",
+// 		"rating" : "7.5/10",
+// 		"image": "./assets/fries/nura.jpg",
+// 		"temp": "medium"
+// 	},
 
-	{
-		"resturant": "Le Solferino",
-		"date": "July 26, 2022",
-		"rating" : "7/10",
-		"image": "./assets/fries/paris.jpg",
-		"temp": "medium"
-	},
+// 	{
+// 		"resturant": "Le Solferino",
+// 		"date": "July 26, 2022",
+// 		"rating" : "7/10",
+// 		"image": "./assets/fries/paris.jpg",
+// 		"temp": "medium"
+// 	},
 
-	{
-		"resturant": "Pastis",
-		"date": "May 23, 2022",
-		"rating" : "8/10",
-		"image": "./assets/fries/pastis.jpg",
-		"temp": "hot"
-	},
+// 	{
+// 		"resturant": "Pastis",
+// 		"date": "May 23, 2022",
+// 		"rating" : "8/10",
+// 		"image": "./assets/fries/pastis.jpg",
+// 		"temp": "hot"
+// 	},
 
-	{
-		"resturant": "Sarah Lawrence Cafeteria",
-		"date": "March 4, 2023",
-		"rating" : "9/10",
-		"image": "./assets/fries/sarahlawrence.jpeg",
-		"temp": "hot"
-	},
+// 	{
+// 		"resturant": "Sarah Lawrence Cafeteria",
+// 		"date": "March 4, 2023",
+// 		"rating" : "9/10",
+// 		"image": "./assets/fries/sarahlawrence.jpeg",
+// 		"temp": "hot"
+// 	},
 
-	{
-		"resturant": "Mainly Burgers",
-		"date": "July 2, 2022",
-		"rating" : "10/10",
-		"image": "./assets/fries/scarbeach.jpg",
-		"temp": "hot"
-	},
-	{
-		"resturant": "University Center",
-		"date": "October 26, 2022",
-		"rating" : "3/10",
-		"image": "./assets/fries/uc1.jpg",
-		"temp": "cold"
-	},
-	{
-		"resturant": "University Center",
-		"date": "March 8, 2023",
-		"rating" : "7/10",
-		"image": "./assets/fries/uc2.jpeg",
-		"temp": "medium"
-	},
+// 	{
+// 		"resturant": "Mainly Burgers",
+// 		"date": "July 2, 2022",
+// 		"rating" : "10/10",
+// 		"image": "./assets/fries/scarbeach.jpg",
+// 		"temp": "hot"
+// 	},
+// 	{
+// 		"resturant": "University Center",
+// 		"date": "October 26, 2022",
+// 		"rating" : "3/10",
+// 		"image": "./assets/fries/uc1.jpg",
+// 		"temp": "cold"
+// 	},
+// 	{
+// 		"resturant": "University Center",
+// 		"date": "March 8, 2023",
+// 		"rating" : "7/10",
+// 		"image": "./assets/fries/uc2.jpeg",
+// 		"temp": "medium"
+// 	},
 
-	{
-		"resturant": "University Center",
-		"date": "May 11, 2022",
-		"rating" : "7/10",
-		"image": "./assets/fries/uc3.jpg",
-		"temp": "medium"
-	},
+// 	{
+// 		"resturant": "University Center",
+// 		"date": "May 11, 2022",
+// 		"rating" : "7/10",
+// 		"image": "./assets/fries/uc3.jpg",
+// 		"temp": "medium"
+// 	},
 
-	{
-		"resturant": "Vernon Diner",
-		"date": "December 22, 2022",
-		"rating" : "8/10",
-		"image": "./assets/fries/vernon.jpeg",
-		"temp": "hot"
-	},
+// 	{
+// 		"resturant": "Vernon Diner",
+// 		"date": "December 22, 2022",
+// 		"rating" : "8/10",
+// 		"image": "./assets/fries/vernon.jpeg",
+// 		"temp": "hot"
+// 	},
 
-	{
-		"resturant": "Via Vecchia",
-		"date": "March 16, 2023",
-		"rating" : "8/10",
-		"image": "./assets/fries/viavecchia.jpeg",
-		"temp": "hot"
-	},
+// 	{
+// 		"resturant": "Via Vecchia",
+// 		"date": "March 16, 2023",
+// 		"rating" : "8/10",
+// 		"image": "./assets/fries/viavecchia.jpeg",
+// 		"temp": "hot"
+// 	},
 
-	{
-		"resturant": "Figlmüller",
-		"date": "August 2, 2022",
-		"rating" : "9/10",
-		"image": "./assets/fries/vienna.jpg",
-		"temp": "hot"
-	},
+// 	{
+// 		"resturant": "Figlmüller",
+// 		"date": "August 2, 2022",
+// 		"rating" : "9/10",
+// 		"image": "./assets/fries/vienna.jpg",
+// 		"temp": "hot"
+// 	},
 
-	{
-		"resturant": "Washington Square Diner",
-		"date": "October 30, 2022",
-		"rating" : "8/10",
-		"image": "./assets/fries/washdiner.jpg",
-		"temp": "medium"
-	},
+// 	{
+// 		"resturant": "Washington Square Diner",
+// 		"date": "October 30, 2022",
+// 		"rating" : "8/10",
+// 		"image": "./assets/fries/washdiner.jpg",
+// 		"temp": "medium"
+// 	},
 
-	{
-		"resturant": "McDonalds 2",
-		"date": "March 17, 2022",
-		"rating" : "6/10",
-		"image": "./assets/fries/mcdonalds2.jpeg",
-		"temp": "medium"
-	},
+// 	{
+// 		"resturant": "McDonalds 2",
+// 		"date": "March 17, 2022",
+// 		"rating" : "6/10",
+// 		"image": "./assets/fries/mcdonalds2.jpeg",
+// 		"temp": "medium"
+// 	},
 
-	{
-		"resturant": "7th Street Burger 2",
-		"date": "March 19, 2023",
-		"rating" : "9/10",
-		"image": "./assets/fries/7thstreet2.jpeg",
-		"temp": "hot"
-	}
-]
-
-
-function generateImages(fries) {
-
-    const img = document.createElement('img')
-    img.src = fries.image 
-    img.classList.add('gallery-img')
-
-    // create a paragraph element 
-
-    const paragraph = document.createElement('p')
-
-    // change the inner html of the paragraph to the sign name 
-    paragraph.innerHTML = signs.title
+// 	{
+// 		"resturant": "7th Street Burger 2",
+// 		"date": "March 19, 2023",
+// 		"rating" : "9/10",
+// 		"image": "./assets/fries/7thstreet2.jpeg",
+// 		"temp": "hot"
+// 	}
+// ]
 
 
-    const galleryItem = document.createElement("div")
-    galleryItem.appendChild(img)
+// function generateImages(fries) {
 
-    //add the paragraph into the galleryItem div
-    galleryItem.appendChild(paragraph)
+//     const img = document.createElement('img')
+//     img.src = fries.image 
+//     img.classList.add('gallery-img')
 
-    document.querySelector('.gallery').appendChild(galleryItem)
+//     // create a paragraph element 
 
-}
+//     const paragraph = document.createElement('p')
 
-forEach(generateImages)
+//     // change the inner html of the paragraph to the sign name 
+//     paragraph.innerHTML = signs.title
 
 
+//     const galleryItem = document.createElement("div")
+//     galleryItem.appendChild(img)
+
+//     //add the paragraph into the galleryItem div
+//     galleryItem.appendChild(paragraph)
+
+//     document.querySelector('.gallery').appendChild(galleryItem)
+
+// }
+
+// forEach(generateImages)
 
 
 
@@ -269,48 +267,51 @@ forEach(generateImages)
 
 
 
-// // Function to render your items
-// const renderItems = (collection) => {
-// 	// The `ul` where the items will be inserted
-// 	const collectionList = document.getElementById('collection')
 
 
-// 	// Loop through each item in the collection array
-// 	collection.forEach((item) => {
-// 		const listItem = document.createElement('li') // Make the `li`
+// Function to render your items
+const renderItems = (collection) => {
+	// The `ul` where the items will be inserted
+	const collectionList = document.getElementById('collection')
 
 
-// 		// You can make each element inside of that…
-// 		const itemTitle = document.createElement('h2') // Make an `h2`
-// 		itemTitle.innerHTML = item.resturant // Put the JSON title inside
-// 		listItem.appendChild(itemTitle) // And add it to the `li`!
-
-// 		const itemImage = document.createElement('img') // And an image
-// 		itemImage.src = item.image // Set the `src` attribute from the JSON
-// 		listItem.appendChild(itemImage) // And add that too
+	// Loop through each item in the collection array
+	collection.forEach((item) => {
+		const listItem = document.createElement('li') // Make the `li`
 
 
-// 		// This can get annoying, so we can use “template literals” instead
-// 		const itemDetails =
-// 			`
-// 				<p><time>${item.date}</time></p>
-// 				<p><em>${item.rating}</em></p>
-// 				</a>
-// 			`
-// 		listItem.insertAdjacentHTML('beforeend', itemDetails) // Which can we then insert
+		// You can make each element inside of that…
+		const itemTitle = document.createElement('h2') // Make an `h2`
+		itemTitle.innerHTML = item.resturant // Put the JSON title inside
+		listItem.appendChild(itemTitle) // And add it to the `li`!
+
+		const itemImage = document.createElement('img') // And an image
+		itemImage.src = item.image // Set the `src` attribute from the JSON
+		listItem.appendChild(itemImage) // And add that too
+
+
+		// This can get annoying, so we can use “template literals” instead
+		const itemDetails =
+			`
+				<p><time>${item.date}</time></p>
+				<p><time>${item.temp}</time></p>
+				<p><em>${item.rating}</em></p>
+				</a>
+			`
+		listItem.insertAdjacentHTML('beforeend', itemDetails) // Which can we then insert
 
 		
 
-// 		collectionList.appendChild(listItem) // Then add the whole `li` into the `ul`
-// 	})
-// }
+		collectionList.appendChild(listItem) // Then add the whole `li` into the `ul`
+	})
+}
 
 
 
-// // Fetch gets your JSON file…
-// fetch('assets/collection.json')
-// 	.then(response => response.json())
-// 	.then(collection => {
-// 		// And passes the data to the function, above!
-// 		renderItems(collection.reverse()) // In reverse order
-// 	})
+// Fetch gets your JSON file…
+fetch('assets/collection.json')
+	.then(response => response.json())
+	.then(collection => {
+		// And passes the data to the function, above!
+		renderItems(collection.reverse()) // In reverse order
+	})
